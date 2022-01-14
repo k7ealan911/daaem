ff70eb4f385ccecfcb17f07ba59aace4ab6502fc
k7ealan911 <maxmg9@gmail.com>
2022-01-14 07:21:32 +0300

    22222

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
